9d15bdf16e574dbf3d3dd58ae831c7abcfdb2e7a
trehoffman <trehoffman@gmail.com>
2018-04-30 09:53:39 -0400

    sorting, filtering, style

--- js/scanner.js
+++ js/scanner.js
@@ -3,6 +3,8 @@
     this.scanner_histories = [];
     this.scan_clear_buttons = [];
     this.scans = [];
+    this.sortField = 'timestamp';
+    this.sortDirection = 'ASC';
 
     this.init = function () {
         var _scanner = this;
@@ -39,9 +41,58 @@
                 var index = parseInt(e.target.getAttribute('index'));
                 var scan = _scanner.getScan(index);
                 _scanner.removeScan(scan);
+            } else if (e.target.classList.contains('sort')) {
+                var context = _scanner.getContext();
+                var sortField = e.target.getAttribute('sortField');
+                var sortDirection = context.sortDirection;
+
+                if ((typeof (sortDirection) == "undefined" || sortDirection == "" || sortDirection == "ASC") && (sortField == context.sortField)) {
+                    sortDirection = "DESC";
+                } else {
+                    sortDirection = "ASC";
+                }
+
+                context.sortField = sortField;
+                context.sortDirection = sortDirection;
+
+                document.querySelectorAll('.sort-direction').forEach(function (elem) {
+                    var parent = elem.parentElement;
+                    var field = parent.getAttribute('sortField');
+                    var direction = context.sortDirection;
+                    if (field == context.sortField) {
+                        if (direction == "ASC") {
+                            elem.innerHTML = '&#9650;'
+                        } else if (direction == "DESC") {
+                            elem.innerHTML = '&#9660;'
+                        }
+                    } else {
+                        elem.innerHTML = "";
+                    }
+                });
+
+                _scanner.putScans();
+            }
+        });
+
+        document.addEventListener('keyup', function (e) {
+            if (e.target.classList.contains('filter')) {
+                var value = e.target.value.toLowerCase();
+                var rows = document.querySelectorAll('table.scan-history tbody tr');
+                rows.forEach(function (item, index) {
+                    var row_text = item.innerText.toLowerCase();
+                    if (row_text.indexOf(value) > -1) {
+                        item.style.display = "";
+                    } else {
+                        item.style.display = "none";
+                    }
+                });
             }
         });
     };
+
+    this.getContext = function () {
+        return this;
+    }
 
     this.getScan = function (index) {
         var _this = this;
@@ -69,14 +120,45 @@
     };
 
     this.putScans = function () {
-        var scanner_histories = this.scanner_histories;
+        var _this = this;
+        _this.scans.sort(function (a, b) {
+            var field = _this.sortField;
+            var reverse = (_this.sortDirection == "DESC");
+            if (field == "text") {
+                var x = eval('a.' + field).toLowerCase();
+                var y = eval('b.' + field).toLowerCase();
+                if (reverse) {
+                    if (x < y) { return 1; }
+                    if (x > y) { return -1; }
+                } else {
+                    if (x < y) { return -1; }
+                    if (x > y) { return 1; }
+                }
+                return 0;
+            } else {
+                var x = eval('a.' + field);
+                var y = eval('b.' + field);
+
+                if (reverse) {
+                    return y - x;
+                } else {
+                    return x - y;
+                }
+            }
+        });
+
+        var scanner_histories = _this.scanner_histories;
         scanner_histories.forEach(function (elem) {
             elem.innerHTML = "";
         });
-        this.scans.forEach(function (scan, index) {
+
+        _this.scans.forEach(function (scan, index) {
             scanner_histories.forEach(function (elem) {
+                var d = new Date(scan.timestamp);
+                var timestamp = d.getFullYear() + '-' + d.getMonth().padLeft(2) + '-' + d.getDate().padLeft(2) + ' ' + d.getHours().padLeft(2) + ':' + d.getMinutes().padLeft(2) + ':' + d.getSeconds().padLeft(2) + '.' + d.getMilliseconds().padLeft(3);
+
                 elem.innerHTML += '<tr>'
-                    + '<td>' + scan.timestamp + '</td>'
+                    + '<td>' + timestamp + '</td>'
                     + '<td>' + scan.text + '</td>'
                     + '<td>' + '<button type="button" class="remove-scan" index="' + index + '">Remove</button>' + '</td>'
                     + '</tr>';
@@ -112,4 +194,8 @@
         _this.scans = [];
         _this.setScans();
     };
+}
+
+Number.prototype.padLeft = function (n, str) {
+    return Array(n - String(this).length + 1).join(str || '0') + this;
 }
